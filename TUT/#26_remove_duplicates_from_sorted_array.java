1aefd868f19e2dee609527f7ae2ebb6465e54cee
warrentdrew <yipinzhu@outlook.com>
2018-02-19 00:22:11 +0100

    2/19

---
+++ TUT/#26_remove_duplicates_from_sorted_array.java
@@ -0,0 +1,16 @@
+// time: O(n)
+// space: O(1)
+//two pointer: i  index
+// c count (length of the array)
+class Solution {
+    public int removeDuplicates(int[] nums) {
+    	if (nums == null || nums.length == 0) return 0;
+    	int count = 1;
+    	for (int i = 1; i < nums.length; i++) {
+    		if (nums[i - 1] != nums[i]) {
+    			nums[count++] = nums[i];
+    		}
+    	}
+    	return count;
+    }
+}
