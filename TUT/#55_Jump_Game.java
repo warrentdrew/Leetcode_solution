9949f5b79cf3f91f00c8ddbeae2318ed3946ec61
warrentdrew <yipinzhu@outlook.com>
2018-03-12 23:56:09 +0100

    3/12

---
+++ TUT/#55_Jump_Game.java
@@ -0,0 +1,14 @@
+
+// time: O(n)
+// space: O(1)
+// greedy algorithm
+class Solution {
+    public boolean canJump(int[] nums) {
+    	int max = 0;
+    	for (int i = 0; i < nums.length; i++) {
+    		if (i > max) return false;
+    		max = Math.max(nums[i] + i, max);
+    	}
+    	return true;
+    }
+}
